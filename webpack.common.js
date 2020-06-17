107f504563ccf399cd78afd98b24d284418ab692
Przemysław Grochowina <pgrochowina@gmail.com>
2020-06-17 22:57:30 +0200

    initial commit

--- webpack.common.js
+++ webpack.common.js
@@ -63,5 +63,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/przemek.html",
+      inject: true,
+      chunks: ["index"],
+      filename: "przemek.html",
+    }),
   ],
 };
